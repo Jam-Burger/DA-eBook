80549e1d813e0a287dc93bcab6f89acebdde22ac
Jam-Burger <jay40793@gmail.com>
2022-09-24 14:14:31 +0530

    update data

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const courses = [
+    { id: "EL111", instructors: ["Ahlad Kumar", "Srimant Mandal"] },
+    { id: "IC101", instructors: ["Anil Roy"] },
+    { id: "IT112", instructors: ["Bakul Goyal", "Manish Khare"] },
+    { id: "PC110", instructors: ["Sweta Garg"] },
+    { id: "SC107", instructors: ["Nabin Sahu"] },
+    { id: "HM106", instructors: ["Bharani Kolipara"] },
+    { id: "IC121", instructors: ["Sreeja Rajendran", "Ayan Palchaudhuri"] },
+    { id: "IT205", instructors: ["Sunitha V", "Priyanka Singh"] },
+    { id: "IT206", instructors: ["Rachit Chhaya"] },
+    { id: "SC205", instructors: ["Manish Gupta"] },
+    { id: "SC217", instructors: ["Gautam Dutta"] },
+    { id: "CT203", instructors: ["Rajib Lochan Das"] },
+    { id: "HM216", instructors: ["Shefali Jha"] },
+    { id: "IT216", instructors: ["Punit Bhateja, Supantha Pandit"] },
+    { id: "IT227", instructors: ["Amit Mankodi"] },
+    { id: "SC223", instructors: ["Aditya Tatu", "Rahul Muthu"] }
+];
